2ec7a7de61e3832d7bd7e508b75f125518e23ff2
MelvicMario <victor.melo.medina@gmail.com>
2026-06-07 02:30:18 +0200

    Ayuda: variedad +4 por 2+ deportes (v11)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service worker mínimo para que la app sea instalable y funcione offline.
-const CACHE = "trisport-liga-v10";
+const CACHE = "trisport-liga-v11";
 const ASSETS = [
   "./",
   "./index.html",
