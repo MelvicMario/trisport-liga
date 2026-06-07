2203b6f0b37ecc107707134315a4a3fc82d4f950
Victor Melo Medina <victor.melo.medina@gmail.com>
2026-06-07 19:09:15 +0200

    Deploy web 2026-06-07T17:09 (acciones-light)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service worker mínimo para que la app sea instalable y funcione offline.
-const CACHE = "trisport-liga-v29";
+const CACHE = "trisport-liga-v30";
 const ASSETS = [
   "./",
   "./index.html",
